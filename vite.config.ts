4fb7aa146755177558cc134f32307e3b6ba8e251
Lac <laclys@126.com>
2023-01-18 18:39:35 +0800

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+const env = process.argv[process.argv.length - 1]
+
 export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src') // src 路径
     }
   },
+  base: env === 'prod' ? '/orderSys/dist/' : '/',
   plugins: [react()],
 })
